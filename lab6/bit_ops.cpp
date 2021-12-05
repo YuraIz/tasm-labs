93b136fd6c532615455e4a1d3d2db8fb39a1eae6
YuraIz <7516890@gmail.com>
2021-12-05 20:08:42 +0300

    started lab6

--- lab6/bit_ops.cpp
+++ lab6/bit_ops.cpp
@@ -2,23 +2,16 @@
 
 // Возвращает бит номер n числа x.
 // Предполагается 0 <= n <= 31
-unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Взврат -1 бессмысленный и сделан для правильной компиляции
-    return -1;
+unsigned get_bit(unsigned x, unsigned n) {
+    return (x >> n & 1);
 }
 // Выставляет значение v в бите номер n
 // Предполагается что 0 <= n <= 31 и v равно 0 или 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
+void set_bit(unsigned* x, unsigned n, unsigned v) {
+    *x = (*x & ~(1 << n)) ^ (v << n);
 }
 // Меняет значение бита номер n на противоположное.
 // Предполагается что 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+void flip_bit(unsigned* x, unsigned n) {
+    set_bit(x, n, ~get_bit(*x, n) & 1);
 }
-
